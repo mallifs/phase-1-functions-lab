c168918ecc38389c26e9f0ea34a6e36404244ff4
leon <mallifs@icloud>
2022-11-26 20:43:08 +0000

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+let dist;
+function distanceFromHqInBlocks(pickUpLocation){
+    const scuberHQ = 42;
+    if (scuberHQ >= pickUpLocation){
+        dist = scuberHQ - pickUpLocation;
+        return dist;
+    }
+    else if (pickUpLocation > scuberHQ){
+        dist = pickUpLocation - scuberHQ;
+        return dist;
+    }   
+}
+
+function distanceFromHqInFeet(pickUpLocation){
+    const singleBlockInFeet = 264;
+    let newDist = distanceFromHqInBlocks(pickUpLocation);
+    return newDist*singleBlockInFeet;
+    
+}
+
+function distanceTravelledInFeet(start,destination){
+    const singleBlockInFeet = 264;
+    if (start > destination){
+        dist = (start-destination)*singleBlockInFeet;
+        return dist;
+    }
+    else dist = (destination-start)*singleBlockInFeet;
+    return dist;   
+}
+
+function calculatesFarePrice(start, destination){
+    dist = distanceTravelledInFeet(start,destination);
+    if (dist <= 400){
+        return(0);
+    }
+    else if(dist <=2000){
+        return((dist-400)*0.02);
+    }
+    else if (dist <= 2500){
+        return(25);
+    }
+    else return("cannot travel that far");
+
+}
